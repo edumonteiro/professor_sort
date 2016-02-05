ae8e9c3b5bb7b1eece340239a2ff8a39bf5ae8fe
edumonteiro <edunb01@gmail.com>
2016-02-05 12:34:18 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+resource :professor, only: [:new, :create, :edit, :update]
+
+namespace :admin do
+  resources :professors, except: [:destroy]
+  resources :offerings
+  resources :courses
+end
+
+resources :lectures
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
