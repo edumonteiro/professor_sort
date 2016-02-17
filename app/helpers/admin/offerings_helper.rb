205bb64cb3eaa5bcd8aa836376fafba72522b106
edumonteiro <edunb01@gmail.com>
2016-02-17 16:34:22 -0500

    new import_offerings to be required from console

--- app/helpers/admin/offerings_helper.rb
+++ app/helpers/admin/offerings_helper.rb
@@ -1,38 +1,3 @@
 module Admin::OfferingsHelper
   
-  def self.decode_schedule(code)
-    result = []
-    code.split("").each do |value|
-      case value
-      when "a"
-        result.push("Monday")
-      when "b"
-        resul.push("Tuesday")
-      when "c"
-        resul.push("Wednesday")
-      when "d"
-        resul.push("Thursday")
-      when "e"
-        resul.push("Friday")
-      when "1"
-        result.push("8 - 10")
-      when "2"
-        result.push("10 - 12")
-      when "3"
-        result.push("12 - 14")
-      when "4"
-        result.push("14 - 16")
-      when "5"
-        result.push("16 - 18")
-      when "6"
-        result.push("18 - 20")
-      when "7"
-        result.push("19 - 21")
-      when "8"
-        result.push("21 - 23")
-      end
-      result      
-    end
-  end
-
 end
---
+++ db/import/import_offerings.rb
@@ -0,0 +1,330 @@
+# course_id: nil, letter: nil, schedule: nil, professor_id: nil, semester: nil
+# a1  8   10    a monday
+# a2  10  12    b tuesday
+# a3  12  14    c wednesday
+# a4  14  16    d thursday
+# a5  16  18    e friday
+# a6  18  20
+# a7  19  21
+# a8  21  23
+
+
+
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Categorical Data Analysis").id
+aa.letter = "A"
+aa.schedule = "a2c2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Linear Regression Analysis").id
+aa.letter = "A"
+aa.schedule = "b1d1e1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Time Series Analysis").id
+aa.letter = "A"
+aa.schedule = "a6c6"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Survival Analysis").id
+aa.letter = "A"
+aa.schedule = "a1c1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Aplications of Statistics 1").id
+aa.letter = "A"
+aa.schedule = "b1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Aplications of Statistics 1").id
+aa.letter = "B"
+aa.schedule = "a4c4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Biostatistics").id
+aa.letter = "A"
+aa.schedule = "b1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Biostatistics").id
+aa.letter = "B"
+aa.schedule = "b1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Biostatistics").id
+aa.letter = "C"
+aa.schedule = "b4e4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Computations in Statistics 1").id
+aa.letter = "A"
+aa.schedule = "e2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Computations in Statistics 1").id
+aa.letter = "B"
+aa.schedule = "b5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Computations in Statistics 2").id
+aa.letter = "A"
+aa.schedule = "b2d2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Computations in Statistics 2").id
+aa.letter = "B"
+aa.schedule = "b4d4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Design and Analysis of Experiments").id
+aa.letter = "A"
+aa.schedule = "a5c5e5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Demography").id
+aa.letter = "A"
+aa.schedule = "a5c5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "A"
+aa.schedule = "a7c7d7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "B"
+aa.schedule = "a1c1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "C"
+aa.schedule = "a1c1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "D"
+aa.schedule = "a2c2d2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "E"
+aa.schedule = "a7c7d7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "F"
+aa.schedule = "a1c1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "G"
+aa.schedule = "a2c2d2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "H"
+aa.schedule = "a4c4d4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "I"
+aa.schedule = "a8c8d8"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "J"
+aa.schedule = "a2c2d2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "K"
+aa.schedule = "a4c4d4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "L"
+aa.schedule = "a7c7d7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "M"
+aa.schedule = "a8c8d8"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "N"
+aa.schedule = "a5c5d5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Applied Statistics").id
+aa.letter = "O"
+aa.schedule = "a5c5d5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Computational Statistics").id
+aa.letter = "A"
+aa.schedule = "a4c4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Exploratory Statistics").id
+aa.letter = "A"
+aa.schedule = "a2c2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Statistical Inference").id
+aa.letter = "A"
+aa.schedule = "b1d1e1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Introduction to Probability").id
+aa.letter = "A"
+aa.schedule = "a1c1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Introduction to Probability").id
+aa.letter = "B"
+aa.schedule = "a1c1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Statistical Methods 1").id
+aa.letter = "A"
+aa.schedule = "a1c1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Statistical Methods 2").id
+aa.letter = "A"
+aa.schedule = "a4c4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Operations Research").id
+aa.letter = "A"
+aa.schedule = "b4d4e4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "A"
+aa.schedule = "a5c5e5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "B"
+aa.schedule = "a2c2e2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "C"
+aa.schedule = "a7c7e7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "D"
+aa.schedule = "a4c4e4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "E"
+aa.schedule = "a4c4e4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "F"
+aa.schedule = "a5c5e5"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "G"
+aa.schedule = "a7c7e7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "H"
+aa.schedule = "a4c4e4"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "I"
+aa.schedule = "a2c2e2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "J"
+aa.schedule = "a1c1e1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "K"
+aa.schedule = "a7c7e7"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Probability and Statistics").id
+aa.letter = "L"
+aa.schedule = "a1c1e1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Survey Tecniques").id
+aa.letter = "A"
+aa.schedule = "b2d2e2"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Topics in Statistics 1").id
+aa.letter = "A"
+aa.schedule = "b1d1"
+aa.save
+
+aa = Offering.new()
+aa.course_id = Course.find_by(name: "Topics in Statistics 2").id
+aa.letter = "A"
+aa.schedule = "b4d4"
+aa.save
