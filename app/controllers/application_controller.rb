420f1952fb94079a38f5437a0d8ce9f583df56db
FantasticJimmy <dboyjimmy@gmail.com>
2016-02-17 23:36:56 -0500

    professor login, signup, sessions added, restriction added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    protected
+
+    def restrict_access
+      if !current_professor
+        flash[:alert] = "You must log in."
+        redirect_to new_session_path
+      end
+    end
+
+    # def restrict_admin_access
+    #   if current_professor.kind == 'admin'
+    #     flash[:alert] = "You must be an Admin to access here."
+    #     redirect_to movies_path
+    #   end
+    # end
+
+    def current_professor
+      @current_professor ||= Professor.find(session[:professor_id]) if session[:professor_id]
+    end
+
+    helper_method :current_professor
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,28 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      if user.admin
-        redirect_to admin_users_path, notice: "Welcome to the admin page, #{user.full_name}"
-      else
-        redirect_to movies_path,notice: "Welcome back, #{user.full_name}!"
-      end
+    professor = Professor.find_by(email: params[:email])
+    if professor && professor.authenticate(params[:password])
+      session[:professor_id] = professor.id
+      # if user.admin
+      #   redirect_to admin_users_path, notice: "Welcome to the admin page, #{user.full_name}"
+      # else
+      #   redirect_to movies_path,notice: "Welcome back, #{user.full_name}!"
+      # end
+      redirect_to professor_path
     else
       render :new
     end
   end
 
-  def back_to_admin
-    session[:user_id] = session[:me]
-    session[:me] = nil
-    redirect_to admin_users_path
-  end
+  # def back_to_admin
+  #   session[:user_id] = session[:me]
+  #   session[:me] = nil
+  #   redirect_to admin_users_path
+  # end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to movies_path, notice: "Adios!"
-  end
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to movies_path, notice: "Adios!"
+  # end
   
 end
