aa389e0b1b88aaadb74a55cb6a87b62d9c604f49
edumonteiro <edunb01@gmail.com>
2016-02-18 15:31:00 -0500

    current_semester helper function added

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20160218192622) do
 
+  create_table "configurations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "courses", force: :cascade do |t|
     t.string   "name"
     t.integer  "credits"
