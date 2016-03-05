686795196ca7ed37b98827bcb54ecfcbebb25d0f
FantasticJimmy <dboyjimmy@gmail.com>
2016-03-05 13:40:11 -0500

    got rid of speechsys

--- app/assets/javascripts/layouts/index.js
+++ app/assets/javascripts/layouts/index.js
@@ -1,10 +1,10 @@
-$(go);
+// $(go);
 
-$(document).on('page:load',go);
+// $(document).on('page:load',go);
 
-function go(){
-    var msg = new SpeechSynthesisUtterance($('.alert div').text());
-    msg.rate = 1;
-    msg.lang = "es-ES";
-    window.speechSynthesis.speak(msg);
-}
+// function go(){
+//     var msg = new SpeechSynthesisUtterance($('.alert div').text());
+//     msg.rate = 1;
+//     msg.lang = "es-ES";
+//     window.speechSynthesis.speak(msg);
+// }
